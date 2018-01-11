176e957fcf929cd1a1e937acf1066dd5e0c1b2cb
slavomircesla <32729870+slavomircesla@users.noreply.github.com>
2018-01-11 12:41:09 +0100

    Du 11.1.2018 geometry

--- Geometry/src/sk/akademiasovy/main/main.java
+++ Geometry/src/sk/akademiasovy/main/main.java
@@ -2,6 +2,7 @@ package sk.akademiasovy.main;
 
 import sk.akademiasovy.Geometry.Rectangle;
 import sk.akademiasovy.Geometry.Square;
+import sk.akademiasovy.Geometry.Triangle;
 
 /**
  * Created by schoolpc on 11. 1. 2018.
@@ -24,6 +25,17 @@ public class main {
 
         System.out.println("Area of rectan is: "+ rectangle1.getArea() );
         System.out.println("Perimeter of rectang1 is: "+ rectangle1.getPerimeter());
+
+
+        Triangle triangle1 = new Triangle (3, 4, 5);
+
+        System.out.println("Obvod of triangle is: "+ triangle1.getobvod() );
+        System.out.println("Area of triangle is: "+ triangle1.getArea() );
+        System.out.println("Triangle is :" +triangle1.isRectangular());
+        System.out.println("Triangle can or cant be created: " +triangle1.isExistTriangle());
+        System.out.println("Triangle is Equilateral??? :  " +triangle1.isEquilateralTriangle());
+
+
     }
 
 
