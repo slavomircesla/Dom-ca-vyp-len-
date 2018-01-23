11d3c264f7b7b09d5cb8980810c4d8d25bb654a0
slavomircesla <32729870+slavomircesla@users.noreply.github.com>
2018-01-23 12:09:13 +0100

    Java programovanie 1.1

---
+++ java/src/sk/akademiasovy/date/MyDate.java
@@ -0,0 +1,21 @@
+package sk.akademiasovy.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyDate {
+
+    public void today()
+    {
+        Date date = new Date();
+
+    }
+
+
+    public void today2()
+    {
+        Date date = new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("dd-MMM-yyyy, hh:mm:ss");
+        System.out.println(sdf.format(date));
+    }
+}
